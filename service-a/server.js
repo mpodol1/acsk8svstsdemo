8677eb7a3dc7cc366fdfe558978c98c224aeea09
mpodol1 <m_podolski@yahoo.com>
2018-02-21 18:27:15 -0500

    Update server.js

--- service-a/server.js
+++ service-a/server.js
@@ -8,7 +8,7 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(morgan("dev"));
 
-// application -------------------------------------------------------------------
+// application --------------------------------------------------------------------
 app.get('/', function (req, res) {
     console.log('/ request');
     res.sendFile(__dirname + '/public/index.html');
